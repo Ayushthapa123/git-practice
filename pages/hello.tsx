0da027f47508994877c1e9b31c1ebec05d7b006e
Ayush Thapa <thapaaayush115@gmail.com>
2023-01-04 22:02:51 +0545

    fix: add second para

---
+++ pages/hello.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+
+interface IProps {}
+
+/**
+ * @author
+ * @function @Hello file
+ **/
+
+export const Hello: FC<IProps> = (props) => {
+  return (
+    <>
+    <div>
+      <h2>hello from ayush</h2>
+      <p>This is first day text on first/day branch</p>
+      <p>this is first day text second part</p>
+    </div>
+    </>
+    )
+};
