01cfc70c59af665696ce8b674e47d5c9e20cc431
Ayush Thapa <thapaaayush115@gmail.com>
2023-01-04 21:56:09 +0545

    feat: hello.tsx file created

---
+++ pages/hello.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+
+interface IProps {}
+
+/**
+ * @author
+ * @function @Hello file
+ **/
+
+export const Hello: FC<IProps> = (props) => {
+  return (
+    <>
+    <div>
+      <h2>hello from ayush</h2>
+      <p>This is first day text on first/day branch</p>
+    </div>
+    </>
+    )
+};
