1b54a1e31d52054d537fe53ac345827435cccd29
Ayush Thapa <34789051+Ayushthapa123@users.noreply.github.com>
2023-01-04 22:32:03 +0545

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      hello this is me Amrendra
+      hello this is me Amrendra: are you sure?
     </div>
   )
 }
